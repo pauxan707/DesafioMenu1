1bbc44827e505da59b718fc6baafa4d1a1559295
Pau <pauxxann@gmail.com>
2025-09-01 11:20:50 -0300

    add dish

--- src/Dish.cs
+++ src/Dish.cs
@@ -4,7 +4,27 @@ namespace Ucu.Poo.Restaurant
     /// <summary>
     /// Representa un platillo individual en el menú del restaurante.
     /// </summary>
-    public class Dish
+    public class Dish //clase
     {
+        //atributos
+        public string Name { get; set; } 
+        public decimal Price { get; set; }
+        public bool IsVegetarian { get; set; }
+        
+        //met constructor objeto 
+        public Dish(string name, decimal price, bool isVegetarian)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.IsVegetarian = isVegetarian;
+        }
+        
+        //met para mostrar la info del platillo
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Platillo: {Name}");
+            Console.WriteLine($"Precio: {Price}");
+            Console.WriteLine($"Es Vegetariano?: {IsVegetarian}");
+        }
     }
-}
+} 
