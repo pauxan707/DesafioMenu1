ebafbc055aae9861f94cccc5f35420ee188e3411
Marco <marco.carlomagno@correo.ucu.edu.uy>
2025-09-01 17:49:25 -0300

    Waiter

--- src/Waiter.cs
+++ src/Waiter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Ucu.Poo.Restaurant
@@ -6,7 +7,44 @@ namespace Ucu.Poo.Restaurant
     /// Representa un mozo en el restaurante, encargado de atender mesas.
     /// </summary>
     public class Waiter
+
     {
+        public string Name
+        {
+            get;
+            set;
+        }
+        
         private List<Table> assignedTables = new List<Table>();
+
+        public void assignTable(Table table)
+        {
+            this.assignedTables.Add(table);
+        }
+        
+        public void TakeOrder(Table table, Dish dish)
+        {
+            if (!assignedTables.Contains(table))
+            {
+                throw new InvalidOperationException($"La mesa no está asignada al mozo {Name}");
+            }
+        
+            // Agregar el platillo a la orden de la mesa
+            table.AddToOrder(dish);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
 }
